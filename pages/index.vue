c710839ca42cf3586fe4a6b0b265f2492712ee7a
martoast <alexmartos96@gmail.com>
2023-01-14 16:42:10 -0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <iframe src="https://docs.google.com/forms/d/1rsYnOzcSdhTmZoPVQEpZWjc0tzgZK1ca_z2Z_u9QPzE/viewanalytics" width="100%" height="100%" frameborder="0" style="height: 100vh;"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
